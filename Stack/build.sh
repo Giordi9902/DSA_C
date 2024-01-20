be823395124b2c08b2a70642ecdd021fb6f8f18e
Giordi9902 <giorgio99difusco@gmail.com>
2024-01-20 09:09:57 +0100

    [DSA] Update folder architecture

--- Stack/build.sh
+++ Stack/build.sh
@@ -6,7 +6,7 @@ gcc -c source/stack.c -o object/stack.o
 gcc -c source/main.c -o object/main.o
 
 # Link the object files to create the executable
-gcc object/main.o object/tree.o -o object/main
+gcc object/main.o object/stack.o -o object/main
 
 echo "Compilation successful. Executable 'main' is in the 'object' folder."
 
