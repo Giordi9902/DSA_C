249b118ae270a57260297a0e6a54c591296ad963
Giordi9902 <giorgio99difusco@gmail.com>
2024-01-20 09:05:08 +0100

    [DSA] Update folder architecture

---
+++ Lists/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Compile tree.c and create object file
+gcc -c source/list.c -o object/list.o
+
+# Compile main.c and create object file
+gcc -c source/main.c -o object/main.o
+
+# Link the object files to create the executable
+gcc object/main.o object/lists.o -o object/main
+
+echo "Compilation successful. Executable 'main' is in the 'object' folder."
+
